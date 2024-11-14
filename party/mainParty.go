ea549684ed74a915780b02c9ddb0d28aaa783b66
Madpeacher <rainbownatural@yandex.ru>
2024-11-14 21:58:38 +0300

    hello

--- party/mainParty.go
+++ party/mainParty.go
@@ -5,3 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+type Operation func(r rune)
+
+func calculate(a, b int, operation Operation) int {
+	switch operation {
+	case '+':
+
+	}
+}
