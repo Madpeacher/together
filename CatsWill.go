0627f7e9f439cc8db8e35aa67a5219c8fdb8a606
Madpeacher <rainbownatural@yandex.ru>
2024-11-18 23:31:46 +0300

    hello

--- CatsWill.go
+++ CatsWill.go
@@ -10,11 +10,16 @@ type kitten struct {
 	autoritet string
 }
 
+type KittErr struct {
+	error string
+}
+
 func main() {
 	kittens := []kitten{
 		{"Belyash", "joskii"},
 		{"Nigga", "mafia"},
 		{"Lei", "doc"},
+		{"Invalid", "Loshok"},
 	}
 	for _, kitt := range kittens {
 		go Play(kitt)
@@ -22,6 +27,12 @@ func main() {
 	time.Sleep(1 * time.Second)
 }
 
+func checkauto(k kitten) KittErr {
+	if k.autoritet == "Loshok" {
+		return KittErr{"Kiiten cannot be Loshok"}
+	}
+	return KittErr{}
+}
 func Play(k kitten) {
 	fmt.Println(k.name, "with autoritet", k.autoritet, "start to play")
 	time.Sleep(1 * time.Second)
