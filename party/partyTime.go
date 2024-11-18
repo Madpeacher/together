270fe4f0674a1587f541fa3cb5d4cb3b0dee25d3
Akhmed <axmed826@gmail.com>
2024-11-18 22:48:25 +0300

    создали mainParty

--- party/partyTime.go
+++ party/partyTime.go
@@ -6,55 +6,110 @@ import (
 )
 
 type phones struct {
-	name  string
-	model int
+	name_phone string
+	model      int
 }
 
-type guest struct {
+type Guest struct {
 	name       string
 	age        int
 	city       string
 	chromosome int
-	phone      *phones
+	phones
+}
+
+type Chill interface {
+	getChill()
+}
+
+type DJ struct {
+	name     string
+	Playlist []string
+}
+
+type Narco struct {
+	Chromosome int
+	NarcoPrice float64
 }
 
 func main() {
-	Rasul := guest{
+	Rasul := Guest{
 		name:       "Rasul",
 		age:        18,
 		city:       "Buinaksk",
 		chromosome: 46,
-		phone:      &phones{"Xiaomi", 5},
+		phones:     phones{"Xiaomi", 5},
 	}
 
-	Jabrik := guest{
+	Jabrik := Guest{
 		name:       "Jabrik",
 		age:        27,
 		city:       "Buinaksk",
 		chromosome: 45,
-		phone:      &phones{"Iphone", 6},
+		phones:     phones{"Iphone", 6},
 	}
 
-	Alena := guest{
+	Alena := Guest{
 		name:       "Alena",
 		age:        17,
 		city:       "Cursk",
 		chromosome: 46,
-		phone:      &phones{"Xiaomi", 1},
+		phones:     phones{"Xiaomi", 1},
 	}
+	Tom := DJ{
+		name:     "Tom",
+		Playlist: []string{"imagine dragons", "bad omens"},
+	}
+
+	Alex := Narco{
+		Chromosome: 32,
+		NarcoPrice: 5000}
+
+	Alena.Changechromosome()
+
 	//Rasul.Changechromosome()
 	Rasul.Meeting(&Alena)
 	Rasul.Meeting(&Jabrik)
 	printGuest(Rasul)
 	printGuest(Jabrik)
 	printGuest(Alena)
+
+	fmt.Println(Alex)
+	fmt.Println(Tom)
+
+	evrybodyHandsUp(&Alex)
+	evrybodyHandsUp(&Jabrik)
+
+	Alena.giveChromosome(Rasul)
+
 }
 
-func (chelick *guest) Changechromosome() {
+func evrybodyHandsUp(chillman Chill) {
+	fmt.Println("evrybodyHandsUp")
+	guest, ok := chillman.(*Guest)
+	if ok {
+		fmt.Println("поприветствуем нашего гостяя - ", guest.name)
+	}
+}
+
+func (*Narco) getChill() {
+	fmt.Println("Chilll но с опаской")
+}
+
+func (*Guest) getChill() {
+	fmt.Println("Chilllllll broooo")
+}
+
+func (guest1 *Guest) giveChromosome(guest2 Guest) {
+	guest1.chromosome--
+	guest2.chromosome++
+}
+
+func (chelick *Guest) Changechromosome() {
 	chelick.chromosome++
 }
 
-func (meet *guest) Meeting(g *guest) {
+func (meet *Guest) Meeting(g *Guest) {
 	if meet.chromosome == g.chromosome {
 		fmt.Println("ДА КАК ВЫ НИПАНИМАЕТЕ ОНИ ЯБУЦАЦАЦААЦАЦЦААЦА")
 	} else {
@@ -62,10 +117,10 @@ func (meet *guest) Meeting(g *guest) {
 	}
 }
 
-func printGuest(guest guest) {
+func printGuest(guest Guest) {
 	fmt.Println("Имя: " + guest.name)
 	fmt.Println("Лет: " + strconv.Itoa(guest.age))
 	fmt.Println("хромосом: " + strconv.Itoa(guest.chromosome))
-	fmt.Println("с телефоном: " + guest.phone.name)
+	fmt.Println("с телефоном: " + guest.name_phone)
 	fmt.Println()
 }
